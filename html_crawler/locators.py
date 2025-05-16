d7d7eb6b1baaf49213d9e9f7ee46cabfada3996b
김혜영 <hyeyoung.k111@gmail.com>
2025-05-16 14:34:36 +0900

    대/소문자 변경(HOME > Home)

--- html_crawler/locators.py
+++ html_crawler/locators.py
@@ -2,13 +2,13 @@ from selenium.webdriver.common.by import By
 
 CRAWLING_LOCATORS = [
     ("Home", None),
-    ("HOME_YourFeed_Article", (By.CLASS_NAME, "article-preview")),
-    ("HOME_Global_Feed", (By.CSS_SELECTOR, "div.feed-toggle > ul > li:nth-child(2) > a")),
-    ("HOME_New_Post", (By.CSS_SELECTOR, "nav > div > ul > li:nth-child(2) > a")),
-    ("HOME_Settings", (By.CSS_SELECTOR, "nav > div > ul > li:nth-child(3) > a")),
-    ("HOME_MyPage", (By.CSS_SELECTOR, "nav > div > ul > li:nth-child(4) > a")),
-    ("HOME_MyPage_Edit_Profile", (By.CSS_SELECTOR, "nav > div > ul > li:nth-child(4) > a"), (By.CLASS_NAME, "btn-outline-secondary")),
-    ("HOME_MyPage_Article", (By.CSS_SELECTOR, "nav > div > ul > li:nth-child(4) > a"), (By.CLASS_NAME, "article-preview")),
-    ("HOME_MyPage_Favorited_Articles", (By.CSS_SELECTOR, "nav > div > ul > li:nth-child(4) > a"), (By.CSS_SELECTOR, "div.articles-toggle > ul > li:nth-child(2) > a")),
-    ("HOME_NewPost_Publish", (By.CSS_SELECTOR, "nav > div > ul > li:nth-child(2) > a"))    # 5번 데이터 팔로우하는 계정 없음
+    ("Home_YourFeed_Article", (By.CLASS_NAME, "article-preview")),
+    ("Home_Global_Feed", (By.CSS_SELECTOR, "div.feed-toggle > ul > li:nth-child(2) > a")),
+    ("Home_New_Post", (By.CSS_SELECTOR, "nav > div > ul > li:nth-child(2) > a")),
+    ("Home_Settings", (By.CSS_SELECTOR, "nav > div > ul > li:nth-child(3) > a")),
+    ("Home_MyPage", (By.CSS_SELECTOR, "nav > div > ul > li:nth-child(4) > a")),
+    ("Home_MyPage_Edit_Profile", (By.CSS_SELECTOR, "nav > div > ul > li:nth-child(4) > a"), (By.CLASS_NAME, "btn-outline-secondary")),
+    ("Home_MyPage_Article", (By.CSS_SELECTOR, "nav > div > ul > li:nth-child(4) > a"), (By.CLASS_NAME, "article-preview")),
+    ("Home_MyPage_Favorited_Articles", (By.CSS_SELECTOR, "nav > div > ul > li:nth-child(4) > a"), (By.CSS_SELECTOR, "div.articles-toggle > ul > li:nth-child(2) > a")),
+    ("Home_NewPost_Publish", (By.CSS_SELECTOR, "nav > div > ul > li:nth-child(2) > a"))    # 5번 데이터 팔로우하는 계정 없음
 ]
