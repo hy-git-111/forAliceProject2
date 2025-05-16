5e54d2040dfeeac5e3cb7c6f28dcb58e03e49e31
김혜영 <hyeyoung.k111@gmail.com>
2025-05-16 19:48:26 +0900

    locators, html 재 업로드

---
+++ qa-realworld-automation/locators/article_locators.py
@@ -0,0 +1,97 @@
+# 웹 요소 로케이터 목록
+
+## [LoginPage]
+
+LoginPage_LOCATORS = {
+    "root": "#root",
+    "navbar_brand": ".navbar-brand",
+    "home_link": ".nav-link[href='/']",
+    "signin_link": ".nav-link[href='/login']",
+    "signup_link": ".nav-link[href='/register']",
+    "page_title": ".text-xs-center",
+    "need_account_link": ".text-xs-center > a",
+    "email_input": ".form-control[type='email']",
+    "password_input": ".form-control[type='password']",
+    "signin_button": ".btn-primary[type='submit']"
+}# 웹 요소 로케이터 목록
+
+## [LoginPage]
+
+LoginPage_LOCATORS = {
+    "navbar_brand": ".navbar-brand",
+    "home_link": ".nav-link[href='/']",
+    "signin_link": ".nav-link[href='/login']",
+    "signup_link": ".nav-link[href='/register']",
+    "page_title": ".text-xs-center",
+    "need_account_link": "p.text-xs-center > a",
+    "email_input": ".form-control[type='email']",
+    "password_input": ".form-control[type='password']",
+    "signin_button": ".btn-primary[type='submit']"
+}
+
+## [HomePage]
+
+HomePage_LOCATORS = {
+    "navbar_brand": ".navbar-brand",
+    "home_link": ".nav-link[href='/']",
+    "new_post_link": ".nav-link[href='/editor']",
+    "settings_link": ".nav-link[href='/settings']",
+    "profile_link": ".nav-link[href='/@1']",
+    "user_pic": ".user-pic",
+    "your_feed_link": ".nav-link.active",
+    "global_feed_link": ".nav-link:not(.active)",
+    "article_preview": ".article-preview",
+    "popular_tags_section": ".sidebar",
+    "tag_list": ".tag-list"
+}# 웹 요소 로케이터 목록
+
+## [LoginPage]
+
+LoginPage_LOCATORS = {
+    "navbar_brand": ".navbar-brand",
+    "home_link": ".nav-link[href='/']",
+    "signin_link": ".nav-link[href='/login']",
+    "signup_link": ".nav-link[href='/register']",
+    "page_title": ".text-xs-center",
+    "need_account_link": "p.text-xs-center > a",
+    "email_input": ".form-control[type='email']",
+    "password_input": ".form-control[type='password']",
+    "signin_button": ".btn-primary[type='submit']"
+}
+
+## [HomePage]
+
+HomePage_LOCATORS = {
+    "navbar_brand": ".navbar-brand",
+    "home_link": ".nav-link[href='/']",
+    "new_post_link": ".nav-link[href='/editor']",
+    "settings_link": ".nav-link[href='/settings']",
+    "profile_link": ".nav-link[href='/@1']",
+    "user_pic": ".user-pic",
+    "your_feed_link": ".nav-link.active",
+    "global_feed_link": ".nav-link:not(.active)",
+    "article_preview": ".article-preview",
+    "popular_tags_section": ".sidebar",
+    "tag_list": ".tag-list"
+}
+
+## [ArticlePage]
+
+ArticlePage_LOCATORS = {
+    "navbar_brand": ".navbar-brand",
+    "home_link": ".nav-link[href='/']",
+    "new_post_link": ".nav-link[href='/editor']",
+    "settings_link": ".nav-link[href='/settings']",
+    "profile_link": ".nav-link[href='/@1']",
+    "article_title": ".banner h1",
+    "author_image": ".article-meta img",
+    "author_name": ".author",
+    "article_date": ".date",
+    "edit_article_button": ".btn-outline-secondary",
+    "delete_article_button": ".btn-outline-danger",
+    "article_content": ".article-content p",
+    "tag_list": ".tag-list",
+    "comment_textarea": ".comment-form textarea",
+    "comment_author_img": ".comment-author-img",
+    "post_comment_button": ".comment-form button"
+}
---
+++ qa-realworld-automation/locators/signup_locators.py
@@ -0,0 +1,17 @@
+# 웹 요소 로케이터 목록
+
+## [SignupPage]
+
+SignupPage_LOCATORS = {
+    "root": "#root",
+    "navbar_brand": ".navbar-brand",
+    "home_link": "a.nav-link[href='/']",
+    "signin_link": "a.nav-link[href='/login']",
+    "signup_link": "a.nav-link[href='/register']",
+    "signup_header": ".text-xs-center",
+    "have_account_link": "p.text-xs-center > a[href='/login']",
+    "username_input": ".form-control[type='text'][placeholder='Username']",
+    "email_input": ".form-control[type='email'][placeholder='Email']",
+    "password_input": ".form-control[type='password'][placeholder='Password']",
+    "signup_button": ".btn.btn-lg.btn-primary"
+}
