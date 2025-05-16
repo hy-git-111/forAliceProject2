4ee57825edc6af297d54f9ec8c6c9a135a655275
김혜영 <hyeyoung.k111@gmail.com>
2025-05-16 23:51:30 +0900

    json 데이터 출력 오류 수정

--- file_manage.py
+++ file_manage.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 
 # 파일 읽기
 def read_file(file_path):
@@ -7,7 +7,7 @@ def read_file(file_path):
         print(f"{file_path} 파일 읽기 완료")
     return html_text
 
-# 지정된 폴더가 없으면 생성하고 파일을 덮어쓰기기하는 함수
+# Json 저장용: 지정된 폴더가 없으면 생성하고 파일을 덮어쓰기하는 함수
 def save_data_overwrite(data, foldername, filename):
     current_dir = os.getcwd()    
     folder_path = os.path.join(current_dir, foldername)
@@ -18,13 +18,14 @@ def save_data_overwrite(data, foldername, filename):
 
     # 파일 저장
     with open(file_path, "w", encoding="utf-8") as f:
-        if isinstance(data, list):
-            f.write("\n".join(data))
+        if isinstance(data, (list, dict)):
+            json_str = json.dumps(data, ensure_ascii=False, indent=2)
+            f.write(json_str)
         else:
             f.write(data)  # 문자열인 경우 바로 저장
         print(f"{file_path} 파일로 저장되었습니다.")
 
-# 지정된 폴더가 없으면 생성하고 파일을 누적하여 저장하는 함수
+# LLM 응답 저장용: 지정된 폴더가 없으면 생성하고 파일을 누적하여 저장하는 함수
 def save_data_append(data, foldername, filename):
     current_dir = os.getcwd()    
     folder_path = os.path.join(current_dir, foldername)
