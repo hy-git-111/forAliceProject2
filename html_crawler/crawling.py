505e7d58466c90c10a68d63c94aa8510e110fd9d
김혜영 <hyeyoung.k111@gmail.com>
2025-05-16 19:49:28 +0900

    capture_page() 수정, 변수명 변경

--- html_crawler/crawling.py
+++ html_crawler/crawling.py
@@ -1,22 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from html_crawler.base_page import BasePage
-import time, os
-from file_manage import save_data_overwrite
+from html_crawler.html_base_page import BasePage
+from file_manage import save_data_append
+from html_crawler.user_data import USER_DATA
 
 class CrawlingPage(BasePage):
-    def login(self, ID: str, PASSWORD: str):
-        self.click_element((By.CSS_SELECTOR, "ul > li:nth-child(2) > a"))
-        self.input_text((By.CSS_SELECTOR, "fieldset:nth-child(1) > input"), ID)
-        self.input_text((By.CSS_SELECTOR, "fieldset:nth-child(2) > input"), PASSWORD)
+    # 로그인하는 함수
+    def login(self, EMAIL: str, PASSWORD: str):
+        self.click_element((By.CSS_SELECTOR, "a[href='/login']"))
+        self.input_text((By.CSS_SELECTOR, "input[type='email'][placeholder='Email']"), EMAIL)
+        self.input_text((By.CSS_SELECTOR, "input[type='password'][placeholder='Password']"), PASSWORD)
         self.click_element((By.CLASS_NAME, "btn-primary"))
 
+    # 글 게시하는 함수
     def publish_article(self, title, description, article):
-        self.input_text((By.CLASS_NAME, "form-control-lg"), title)
-        self.input_text((By.CSS_SELECTOR, "fieldset:nth-child(2) > input"), description)
-        self.input_text((By.CSS_SELECTOR, "fieldset:nth-child(3) > textarea"), article)
-        self.click_element((By.CLASS_NAME), "btn-primary")
+        self.input_text((By.CSS_SELECTOR, "input[type='text'][placeholder='Article Title']"), title)
+        self.input_text((By.CSS_SELECTOR, "input[type='text'][placeholder='What's this article about?']"), description)
+        self.input_text((By.CSS_SELECTOR, "textarea[placeholder='Write your article (in markdown)']"), article)
 
+    # html body의 모든 태그를 반환하는 함수
     def get_html_tags(self):
         tags = self.driver.execute_script("""
             return (function getNodePath(node, depth = 0) {
@@ -47,39 +49,41 @@ class CrawlingPage(BasePage):
 
         return tags
 
-    def capture_page(self, title, click_1=None, click_2=None, click_3 = None): 
+    # 로그인 이후 진입 가능한 페이지를 크롤링하여 반환하는 함수
+    def capture_page(self, title, login_required, locator_1=None, locator_2=None, locator_3 = None): 
         url = "http://localhost:4100/"
         self.driver.get(url)
-        self.login("5@5", "5")
+
+        if login_required == True:
+            self.login(USER_DATA["email"], USER_DATA["password"])
         
-        if click_1:
-            self.click_element(click_1)
+        if locator_1:
+            self.click_element(locator_1)
             self.wait_for_page_load()
-            time.sleep(1)
-            if click_2:
-                self.click_element(click_2)
+
+            if locator_2:
+                self.click_element(locator_2)
                 self.wait_for_page_load()
 
-                if click_3:
+                if locator_3:
                     self.publish_article("타이틀", "요약", "내용")
+                    self.click_element(locator_3)
                     self.wait_for_page_load()
 
-        html_data = (f"<!-- {title} -->\n{self.get_html_tags()}")
+        html_data = f"<!-- {title} -->\n{self.get_html_tags()}"
         return html_data
+    
+def capture_page(title, login_required, locator_1=None, locator_2=None, locator_3=None):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new")
+    options.add_argument("--window-size=1920,1080")
 
-def capture_page(title, click_1=None, click_2=None, click_3=None):
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=options)
     crawling = CrawlingPage(driver)
 
     try:
-        html_data = crawling.capture_page(title, click_1, click_2, click_3)
+        html_data = crawling.capture_page(title, login_required, locator_1, locator_2, locator_3)
         filename = title + ".html"
-        save_data_overwrite(html_data, "html_data", filename)
+        save_data_append(html_data, "html_data", filename)
     finally:
-        time.sleep(1)
         driver.quit()
-    
-
-        
-
-
