724ad46d8130db1f35a22344f4e492b021c14b39
김혜영 <hyeyoung.k111@gmail.com>
2025-05-16 19:40:29 +0900

    파일 이름 변경 및 예외처리 추가

--- html_crawler/html_base_page.py
+++ html_crawler/html_base_page.py
@@ -8,12 +8,18 @@ class BasePage:
         # self.wait = WebDriverWait(driver, 10)    
 
     def click_element(self, locator):
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(locator)).click()
+        try:
+            WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(locator)).click()
+        except Exception as e:
+            print(e)
 
     def input_text(self, locator, text):
-        element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(locator))
-        element.clear()
-        element.send_keys(text)    
+        try:
+            element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(locator))
+            element.clear()
+            element.send_keys(text)    
+        except Exception as e:
+            print(e)
     
     def wait_for_page_load(self, timeout=10):
         WebDriverWait(self.driver, timeout).until(
