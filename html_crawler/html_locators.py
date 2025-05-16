b0e34607d10bf4909640d0e36bce733dbef654b9
김혜영 <hyeyoung.k111@gmail.com>
2025-05-16 19:40:49 +0900

    파일 이름 변경 및 로케이터 수정

---
+++ html_crawler/html_locators.py
@@ -0,0 +1,19 @@
+from selenium.webdriver.common.by import By
+
+CRAWLING_LOCATORS = [
+    ("login", False, (By.CSS_SELECTOR, "a[href='/login']")),    # Home > Login
+    ("signup", False, (By.CSS_SELECTOR, "a[href='/login']"), (By.CSS_SELECTOR, "a.nav-link[href='/register']")),    # Home > Login > Sign up
+
+    ("home", True, None), # Home > Login > YourFeed
+    ("home", True, (By.XPATH, "//a[normalize-space()='Global Feed']")),  # Home > Login > Global Feed
+    ("new_post", True, (By.CSS_SELECTOR, "a[href='/editor']")),    # Home > Login > New Post
+    ("settings", True, (By.CSS_SELECTOR, "a[href='/settings']")),    # Home > Login > Settings
+
+    ("profile", True, (By.CSS_SELECTOR, "a[href^='/@']")),     # Home > Login > MyPage
+    ("profile", True, (By.CSS_SELECTOR, "a[href^='/@']"), (By.CSS_SELECTOR, "a[href$='favorites']")),   # Home > Login > MyPage > Favorited Articles
+    ("profile", True, (By.CSS_SELECTOR, "a[href^='/@']"), (By.CLASS_NAME, "btn-outline-secondary")),  # Home > Login > MyPage > Edit Profile
+    
+    ("article", True, None, (By.CSS_SELECTOR, "a[href='/editor']"), (By.CLASS_NAME, "btn-primary")),    # Home > Login > New Post > Publish > Written Article
+    ("article", True, (By.CLASS_NAME, "article-preview")),    # Home > Login > YourFeed > Article
+    ("article",True, (By.CSS_SELECTOR, "a[href^='/@']"), (By.CLASS_NAME, "article-preview")),    # Home > Login > MyPage > Article
+]
